97b30081581856f5d4364bd824a77e2d35cb7f49
Nicholas Bradach <nick.bradach33@gmail.com>
2022-01-10 19:16:24 +0100

    First commit ever

---
+++ twitAnalizer/src/test/java/it/univpm/twitAnalizer/TwitAnalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.twitAnalizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitAnalizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
