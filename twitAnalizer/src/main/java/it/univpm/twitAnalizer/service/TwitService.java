433c6bd05de85bc48667e5e9b16420965371300c
Nicholas Bradach <nick.bradach33@gmail.com>
2022-01-11 18:20:12 +0100

    Updated TwitService

--- twitAnalizer/src/main/java/it/univpm/twitAnalizer/service/TwitService.java
+++ twitAnalizer/src/main/java/it/univpm/twitAnalizer/service/TwitService.java
@@ -1,5 +1,10 @@
 package it.univpm.twitAnalizer.service;
 
-public interface TwitService {
+import com.fasterxml.jackson.databind.util.JSONPObject;
 
+import it.univpm.twitAnalizer.model.TwitModel;
+
+public interface TwitService {
+	public abstract JSONPObject getTwit(TwitModel tweet);
+	public abstract JSONPObject statistics();
 }
