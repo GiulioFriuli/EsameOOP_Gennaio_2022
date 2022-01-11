833f1e9160a6d58d0c8cac41ee44a04d6abb0978
Nicholas Bradach <nick.bradach33@gmail.com>
2022-01-11 12:31:53 +0100

    Added variables and getters & setters in TwitModel

--- twitAnalizer/src/main/java/it/univpm/twitAnalizer/model/TwitModel.java
+++ twitAnalizer/src/main/java/it/univpm/twitAnalizer/model/TwitModel.java
@@ -1,5 +1,24 @@
 package it.univpm.twitAnalizer.model;
 
-public class TwitModel {
+import java.util.Date;
 
+public class TwitModel {
+	private String placeId;
+	private Date created;
+	
+	public String getPlaceId() {
+		return placeId;
+	}
+	
+	public void setPlaceId(String placeId) {
+		this.placeId = placeId;
+	}
+	
+	public Date getCreated() {
+		return created;
+	}
+	
+	public void setCreated(Date created) {
+		this.created = created;
+	}
 }
